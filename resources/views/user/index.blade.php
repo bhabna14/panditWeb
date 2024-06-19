52b65296e4c9a8ea1e42e9b8dfc8c7c2c5ca1f65
bhabna14 <166595612+bhabna14@users.noreply.github.com>
2024-06-19 11:00:34 +0530

    update

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -10,8 +10,8 @@
     <div class="container">
       <div class="row">
         <div class="col-md-12 text-center">
-          <h1 data-aos="fade-left" data-aos-delay="500">Reliable Pandit Booking For Every <br> Religious Event</h1>
-          <p data-aos="fade-right" data-aos-delay="500">Experienced Pandits For Every Occasion, Just a Click Away</p>
+          <h1 data-aos="fade-up" data-aos-delay="500">Reliable Pandit Booking For Every <br> Religious Event</h1>
+          <p data-aos="fade-up" data-aos-delay="500">Experienced Pandits For Every Occasion, Just a Click Away</p>
           <a href="" class="book-now-btn" data-aos="fade-up" data-aos-delay="500">Book Now</a>
           
         </div>
@@ -21,7 +21,7 @@
  <section>
   <div class="container">
       <div class="row" style="margin-top:60px">
-          <div class="col-md-6" data-aos="fade-right" data-aos-delay="500">
+          <div class="col-md-6" data-aos="fade-up" data-aos-delay="500">
               
               <div class="img-group">
                   <div class="img-group-inner">
@@ -33,7 +33,7 @@
                   <img src="https://metropolitanhost.com/themes/themeforest/html/maharatri/assets/img/about-group1/3.jpg" alt="about">
               </div>
           </div>
-          <div class="col-md-6" data-aos="fade-left" data-aos-delay="500">
+          <div class="col-md-6" data-aos="fade-up" data-aos-delay="500">
               <div class="section-title mb-0 text-start">
                   <p class="subtitle">EDUCATION FOR ALL RURAL CHILDREN</p>
                   <h4 class="title">We are a Hindu that believe in Ram</h4>
@@ -89,6 +89,11 @@
                                
                                 <div class="upcoming-event-img">
                                     <img src="https://vihara.themerex.net/wp-content/uploads/2018/12/post-15-copyright-520x424.jpg" alt="Avatar" class="image">
+                                    {{-- <div class="overlay">
+                                        <a href="#" class="icon" title="User Profile">
+                                            <i class="fa fa-user"></i>
+                                        </a>
+                                    </div> --}}
                                 </div>
                             </div>
                           
@@ -112,6 +117,11 @@
                                
                                 <div class="upcoming-event-img">
                                     <img src="{{asset('front-assets/img/masthead/post-11-copyright-520x424.jpg')}}" alt="Avatar" class="image">
+                                    {{-- <div class="overlay">
+                                        <a href="#" class="icon" title="User Profile">
+                                            <i class="fa fa-user"></i>
+                                        </a>
+                                    </div> --}}
                                 </div>
                             </div>
                           
@@ -134,6 +144,9 @@
         </div>
     </section>
 
+ 
+  
+   
     <section class="layout-pt-md layout-pb-md">
         <div class="container">
             <div data-anim="" data-aos="fade-up" class="row y-gap-20 mb-30 justify-between items-end">
@@ -205,8 +218,10 @@
                     </div>
                   
                 </div>
-              </div>
-              <div class="row mb-30" data-aos="fade-up" data-aos-delay="500">
+
+              
+            </div>
+            <div class="row mb-30" data-aos="fade-up" data-aos-delay="500">
                 <div class="col-md-4 col-12 mb-20">
                   <div class="row">
                     <div class="col-md-4 col-4">
@@ -258,12 +273,291 @@
                     </div>
                   
                 </div>
-              </div>
-            
+
+              
+            </div>
+        
+
         </div>
     </section>
 
+    <section class="special-pooja-bg layout-pt-md layout-pb-md">
+        <div class="container">
+            <div data-anim="" data-aos="fade-up" data-aos-delay="500" class="row y-gap-20 justify-between items-end">
+                <div class="col-auto">
+                    <div class="sectionTitle -md">
+                        <h2 class="sectionTitle__title">Special Pooja</h2>
+                        <p class=" sectionTitle__text mt-5 sm:mt-0">These are few upcoming Pooja for you to do</p>
+                    </div>
+                </div>
+
+                <div class="col-auto md:d-none">
+
+                    <a href="{{ url('pooja-list')}}" class="button -md -blue-1 all-ppja-btn">
+                        View All Pooja <div class="icon-arrow-top-right ml-15"></div>
+                    </a>
+
+                </div>
+            </div>
+
+            <div class = "row" data-aos="fade-up" data-aos-delay="500">
+                @foreach ($otherpoojas as $otherpooja)
+                <div class="col-md-4 pandit-card">
+                    <a href="{{url('puja-details')}}"> 
+                        <div class="card" data-state="#pooja">
+                            <div class="card-header">
+                                <img class="card-pooja" src="{{ asset('assets/img/'.$otherpooja->pooja_photo) }}" alt="image">
+                            </div>
+                            <div class="pooja-head">
+                                <h5>{{$otherpooja->pooja_name}}</h5>
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+                                <div style="text-align: center">
+                                    {{-- <h6>(12-03-2024)</h6> --}}
+                                </div>
+                                {{-- <button class="contact-me">Book Now</button> --}}
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                @endforeach
+              
+            </div>
+
+           
+        </div>
+    </section>
+
+ 
+
+
+    <section class="testimonial-bg">
+        <div data-anim-wrap class="container">
+          <div  class="row justify-center text-center">
+            <div class="col-auto">
+              <div class="sectionTitle text-white">
+                <h2 class="sectionTitle__title">Customer Reviews</h2>
+                <p class=" sectionTitle__text mt-5 sm:mt-0">These popular destinations have a lot to offer</p>
+              </div>
+            </div>
+          </div>
+  
+          <div  class="overflow-hidden pt-60 lg:pt-40 sm:pt-30 js-section-slider" data-gap="30" data-slider-cols="xl-3 lg-3 md-2 sm-1 base-1">
+            <div class="swiper-wrapper">
+  
+              <div class="swiper-slide">
+                <div class="testimonials -type-1 bg-white rounded-4 pt-40 pb-30 px-40">
+                  
+                  <p class="testimonials__text lh-18 fw-500 text-dark-1">&quot;Our family was traveling via bullet train between cities in Japan with our luggage - the location for this hotel made that so easy. Agoda price was fantastic.&quot;</p>
+  
+                  <div class="pt-20 mt-28 border-top-light">
+                    <div class="row x-gap-20 y-gap-20 items-center">
+                      <div class="col-auto">
+                        <img class="size-60" src="img/avatars/1.png" alt="image">
+                      </div>
+  
+                      <div class="col-auto">
+                        <div class="text-15 fw-500 lh-14">Courtney Henry</div>
+                        <div class="text-14 lh-14 text-light-1 mt-5">Web Designer</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+  
+              <div class="swiper-slide">
+                <div class="testimonials -type-1 bg-white rounded-4 pt-40 pb-30 px-40">
+                  
+                  <p class="testimonials__text lh-18 fw-500 text-dark-1">&quot;Our family was traveling via bullet train between cities in Japan with our luggage - the location for this hotel made that so easy. Agoda price was fantastic.&quot;</p>
+  
+                  <div class="pt-20 mt-28 border-top-light">
+                    <div class="row x-gap-20 y-gap-20 items-center">
+                      <div class="col-auto">
+                        <img class="size-60" src="img/avatars/1.png" alt="image">
+                      </div>
+  
+                      <div class="col-auto">
+                        <div class="text-15 fw-500 lh-14">Courtney Henry</div>
+                        <div class="text-14 lh-14 text-light-1 mt-5">Web Designer</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+  
+              <div class="swiper-slide">
+                <div class="testimonials -type-1 bg-white rounded-4 pt-40 pb-30 px-40">
+                  
+                  <p class="testimonials__text lh-18 fw-500 text-dark-1">&quot;Our family was traveling via bullet train between cities in Japan with our luggage - the location for this hotel made that so easy. Agoda price was fantastic.&quot;</p>
+  
+                  <div class="pt-20 mt-28 border-top-light">
+                    <div class="row x-gap-20 y-gap-20 items-center">
+                      <div class="col-auto">
+                        <img class="size-60" src="img/avatars/1.png" alt="image">
+                      </div>
+  
+                      <div class="col-auto">
+                        <div class="text-15 fw-500 lh-14">Courtney Henry</div>
+                        <div class="text-14 lh-14 text-light-1 mt-5">Web Designer</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+  
+              <div class="swiper-slide">
+                <div class="testimonials -type-1 bg-white rounded-4 pt-40 pb-30 px-40">
+                  
+                  <p class="testimonials__text lh-18 fw-500 text-dark-1">&quot;Our family was traveling via bullet train between cities in Japan with our luggage - the location for this hotel made that so easy. Agoda price was fantastic.&quot;</p>
+  
+                  <div class="pt-20 mt-28 border-top-light">
+                    <div class="row x-gap-20 y-gap-20 items-center">
+                      <div class="col-auto">
+                        <img class="size-60" src="img/avatars/1.png" alt="image">
+                      </div>
+  
+                      <div class="col-auto">
+                        <div class="text-15 fw-500 lh-14">Courtney Henry</div>
+                        <div class="text-14 lh-14 text-light-1 mt-5">Web Designer</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+  
+              <div class="swiper-slide">
+                <div class="testimonials -type-1 bg-white rounded-4 pt-40 pb-30 px-40">
+                  
+                  <p class="testimonials__text lh-18 fw-500 text-dark-1">&quot;Our family was traveling via bullet train between cities in Japan with our luggage - the location for this hotel made that so easy. Agoda price was fantastic.&quot;</p>
+  
+                  <div class="pt-20 mt-28 border-top-light">
+                    <div class="row x-gap-20 y-gap-20 items-center">
+                      <div class="col-auto">
+                        <img class="size-60" src="img/avatars/1.png" alt="image">
+                      </div>
+  
+                      <div class="col-auto">
+                        <div class="text-15 fw-500 lh-14">Courtney Henry</div>
+                        <div class="text-14 lh-14 text-light-1 mt-5">Web Designer</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+  
+            </div>
+          </div>
+  
+         
+        </div>
+    </section>
+
+  {{--cta---}}
+  <div class="section-cta-custom pt-0">
+    <div class="container">
+        <div class="section-title text-center">
+            <p class="subtitle">WAYS WE CAN HELP</p>
+            <h4 class="title">Pandits Ready To Help</h4>
+        </div>
+        <div class="row align-items-center position-relative">
+            <div class="col-md-6 col-12 mb-4">
+                <div class="sigma_cta primary-bg">
+                    <img class="cta-img-left" src="https://metropolitanhost.com/themes/themeforest/html/maharatri/assets/img/cta/1.png" alt="cta">
+                    <div class="sigma_cta-content">
+                        <h4 class="text-white">+91 9776888887</h4>
+                    </div>
+                </div>
+            </div>
+            <span class="sigma_cta-sperator d-lg-flex">or</span>
+            <div class="col-md-6 col-12">
+                <div class="sigma_cta primary-bg">
+                    <div class="sigma_cta-content">
+                        <h4 class="text-white">contact@33crores.com</h4>
+                    </div>
+                    <img class="cta-img-right" src="https://metropolitanhost.com/themes/themeforest/html/maharatri/assets/img/cta/2.png" alt="cta">
+                </div>
+            </div>
+        </div>
+    </div>
+  </div>
+
+  
+    <section class="pt-60 custmer-count">
+      <div class="container">
+        <div class="border-bottom-light pb-40">
+          <div class="row y-gap-30 justify-center text-center">
+  
+            <div class="col-xl-3 col-6">
+              <div class="text-40 lg:text-30 lh-13 fw-600 counter">4,958</div>
+              <div class="text-14 lh-14 text-light-1 mt-5 ">Pandits</div>
+            </div>
+  
+            <div class="col-xl-3 col-6">
+              <div class="text-40 lg:text-30 lh-13 fw-600 counter">2,869</div>
+              <div class="text-14 lh-14 text-light-1 mt-5">Total Pooja</div>
+            </div>
+  
+            <div class="col-xl-3 col-6">
+              <div class="text-40 lg:text-30 lh-13 fw-600">2M</div>
+              <div class="text-14 lh-14 text-light-1 mt-5">Happy customers</div>
+            </div>
+  
+            <div class="col-xl-3 col-6">
+              <div class="text-40 lg:text-30 lh-13 fw-600">574,974</div>
+              <div class="text-14 lh-14 text-light-1 mt-5">Our Volunteers</div>
+            </div>
+  
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="section-bg pt-80 pb-80 md:pt-40 md:pb-40">
+
+
+        <div class="container">
+            <div class="row y-gap-30 items-center justify-between">
+                <div  class="col-xl-5 col-lg-6" data-aos="fade-up" data-aos-delay="500">
+                    <h2 class="text-30 lh-15">Download the App</h2>
+                    <p class="text-dark-1 pr-40 lg:pr-0 mt-15 sm:mt-5">Book in advance or last-minute with 33Crores.
+                        Receive instant confirmation. Access your booking info offline.</p>
+
+                    <div class="row y-gap-20 items-center pt-30 sm:pt-10">
+                        <div class="col-auto">
+                            <div class="d-flex items-center px-20 py-10 rounded-8 border-white-15 text-white bg-dark-3">
+                                <div class="icon-apple text-24"></div>
+                                <div class="ml-20">
+                                    <div class="text-14">Download on the</div>
+                                    <div class="text-15 lh-1 fw-500">Apple Store</div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-auto" >
+                            <a href="https://play.google.com/store/apps/details?id=com.croresadmin.shopifyapp"
+                                target="_blank"
+                                class="d-flex items-center px-20 py-10 rounded-8 border-white-15 text-white bg-dark-3">
+                                <div class="icon-play-market text-24"></div>
+                                <div class="ml-20">
+
+                                    <div class="text-14">Get in on</div>
+                                    <div class="text-15 lh-1 fw-500">Google Play</div>
+
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+                <div  class="col-lg-6" data-aos="fade-up" data-aos-delay="500">
+                    <img src="{{ asset('front-assets/img/app/1.png') }}" alt="image">
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+   
 @endsection
 
 @section('scripts')
+
 @endsection
