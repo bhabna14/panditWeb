b042ad2eb8f7aed18bf32d56288142c767a7acab
bhabna14 <166595612+bhabna14@users.noreply.github.com>
2024-07-30 15:32:32 +0530

    commit

--- app/Models/PanditLogin.php
+++ app/Models/PanditLogin.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Laravel\Sanctum\HasApiTokens;
+
+
 
 class PanditLogin extends Authenticatable
 {
+   
+    use HasApiTokens, HasFactory;
     use Notifiable;
     protected $table = 'pandit_login';
 
