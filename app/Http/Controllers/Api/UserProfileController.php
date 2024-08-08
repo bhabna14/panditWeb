227d8242cfdae1423c81740d2c3ea2295ce517ba
bhabna14 <166595612+bhabna14@users.noreply.github.com>
2024-08-08 18:33:57 +0530

    commit

--- app/Http/Controllers/Api/UserProfileController.php
+++ app/Http/Controllers/Api/UserProfileController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Booking;
+use App\Models\Payment;
+
 use App\Models\Profile;
 use App\Models\Poojalist;
 
