446a0a1ab590cab55a6bf1159407ade57f488d61
bhabna14 <166595612+bhabna14@users.noreply.github.com>
2024-08-23 17:48:29 +0530

    commit

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -72,7 +72,7 @@ class Booking extends Model
     }
     public function ratings()
     {
-        return $this->hasMany(Rating::class, 'booking_id' ,'booking_id');
+        return $this->belongsTo(Rating::class, 'booking_id' ,'booking_id');
     }
 
     public function payment()
