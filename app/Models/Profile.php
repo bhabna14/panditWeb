f1233aeedaa580ecc08b9106737de2d0b270860d
bhabna14 <166595612+bhabna14@users.noreply.github.com>
2024-08-28 13:05:03 +0530

    commit

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -9,7 +9,7 @@ class Profile extends Model
 {
     use HasFactory;
     protected $table = "pandit_profile";
-    protected $fillable = ['pandit_id', 'otp', 'phonenumber', 'title', 'name', 'email', 'whatsappno', 'bloodgroup', 'profile_photo', 'maritalstatus', 'language'];
+    protected $fillable = ['pandit_id', 'otp', 'phonenumber', 'title', 'name', 'email', 'whatsappno', 'bloodgroup', 'profile_photo', 'maritalstatus', 'language','agree'];
 
     public function poojadetails()
     {
