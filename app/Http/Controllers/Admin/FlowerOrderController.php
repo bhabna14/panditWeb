404b0a7efa8dd777b87a1ba16bb58f56430a8d7b
bhabna14 <166595612+bhabna14@users.noreply.github.com>
2024-11-04 12:31:04 +0530

    commit

--- app/Http/Controllers/Admin/FlowerOrderController.php
+++ app/Http/Controllers/Admin/FlowerOrderController.php
@@ -17,7 +17,7 @@ class FlowerOrderController extends Controller
     {
         $orders = Order::whereNull('request_id')
                        ->with(['flowerRequest', 'subscription', 'flowerPayments', 'user','flowerProduct','address'])
-                       ->orderBy('id', 'asc')
+                       ->orderBy('created_at', 'asc')
                        ->get();
                        
         return view('admin.flower-order.manage-flower-order', compact('orders'));
