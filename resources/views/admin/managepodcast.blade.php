bb66cdbd26bd12b339ac70a9bc52bc6fb004ec17
bhabna14 <166595612+bhabna14@users.noreply.github.com>
2024-11-08 13:32:25 +0530

    donr

--- resources/views/admin/managepodcast.blade.php
+++ resources/views/admin/managepodcast.blade.php
@@ -94,7 +94,7 @@
                                                     <td class="border-bottom-0">
                                                         <div class="form-check form-switch">
                                                             <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckChecked" checked>
-                                                            <label class="form-check-label" for="flexSwitchCheckChecked">Checked switch checkbox input</label>
+                                                            <label class="form-check-label" for="flexSwitchCheckChecked">Active</label>
                                                           </div>
                                                         {{ $podcast->status }}</td>
 
