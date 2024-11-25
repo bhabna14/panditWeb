4e03ca3047a900793c9119363dd019679a11441f
bhabna14 <166595612+bhabna14@users.noreply.github.com>
2024-11-25 13:25:49 +0530

    commit

--- resources/views/admin/flower-order/show-customer-details.blade.php
+++ resources/views/admin/flower-order/show-customer-details.blade.php
@@ -163,12 +163,16 @@
                 
                 
                                                                     </td>
-                                                                    <td>
-                                                                        <strong>Address:</strong> {{ $order->address->area ?? "" }}<br>
-                                                                        <strong>City:</strong> {{ $order->address->city ?? ""}}<br>
-                                                                        <strong>State:</strong> {{ $order->address->state ?? ""}}<br>
-                                                                        <strong>Zip Code:</strong> {{ $order->address->pincode ?? "" }}
-                                                                    </td>
+                                                                    
+                                                                        <td>
+                                                                            <strong>Address:</strong> {{ $order->address->apartment_flat_plot ?? "" }}, {{ $order->address->locality ?? "" }}<br>
+                                                                            <strong>Landmark:</strong> {{ $order->address->landmark ?? "" }}<br>
+                
+                                                                            <strong>City:</strong> {{ $order->address->city ?? ""}}<br>
+                                                                            <strong>State:</strong> {{ $order->address->state ?? ""}}<br>
+                                                                            <strong>Pin Code:</strong> {{ $order->address->pincode ?? "" }}
+                                                                        </td>
+                                                                   
                                                                     {{-- <td>{{ $order->total_price }}</td> --}}
                                                                     <td>{{ number_format($order->total_price, 2) }}</td>
                 
