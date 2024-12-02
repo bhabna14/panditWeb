84130b9980215e887a36e91865e530875876f57f
bhabna14 <166595612+bhabna14@users.noreply.github.com>
2024-12-02 18:39:54 +0530

    commit

--- app/Http/Controllers/Notification/PushNotificationController.php
+++ app/Http/Controllers/Notification/PushNotificationController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\UserUnauthorisedDevices;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Http;
 
 class PushNotificationController extends Controller
 {
