af37790604b681386fe10f128b80aa60f7e390a4
bhabna14 <166595612+bhabna14@users.noreply.github.com>
2024-12-04 15:56:35 +0530

    commit

--- resources/views/admin/flower-request/manage-flower-request.blade.php
+++ resources/views/admin/flower-request/manage-flower-request.blade.php
@@ -125,6 +125,7 @@
                                                     <th>Flower Items</th>
                                                    
                                                     <th>Status</th>
+                                                    <th>Deliery Status</th>
                                                     <th>Price</th>
                                                     <th>Actions</th>
                                                     <th>Assigned Rider</th>
@@ -158,6 +159,8 @@
                                                                 <p>Payment Completed</p>
                                                             @endif
                                                         </td>
+                                                        <td>{{ $request->order->delivery->delivery_status }}</td>
+
                                                         <td>
                                                             @if($request->order && $request->order->total_price)
                                                                 <span>Total Price : {{ $request->order->total_price }} <br>
