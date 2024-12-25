4c453db47713c729aabbfa887f09b8540a3321d6
bhabna14 <166595612+bhabna14@users.noreply.github.com>
2024-12-25 16:47:11 +0530

    remove cred

--- app/Http/Controllers/Api/FlowerBookingController.php
+++ app/Http/Controllers/Api/FlowerBookingController.php
@@ -350,6 +350,7 @@ class FlowerBookingController extends Controller
 
         \Log::info('Email sent successfully to all recipients.');
 
+       
       
         // Return a successful response with flower request details
         return response()->json([
