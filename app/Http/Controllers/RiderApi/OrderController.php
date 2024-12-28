8efe1ac61af296d65bcf0e283c5fd654fd530338
bhabna14 <166595612+bhabna14@users.noreply.github.com>
2024-12-28 20:08:26 +0530

    commit

--- app/Http/Controllers/RiderApi/OrderController.php
+++ app/Http/Controllers/RiderApi/OrderController.php
@@ -241,6 +241,7 @@ class OrderController extends Controller
             // Fetch delivery history with pending status and today's delivery start time
             $deliveries = DeliveryHistory::where('rider_id', $rider->rider_id)
                 // ->where('delivery_status', 'pending')
+                ->whereDate('created_at', Carbon::today())
                 ->whereHas('deliveryStartHistory', function ($query) use ($today) {
                     $query->whereDate('start_delivery_time', '=', $today);
                 })
