2de445a47f5c7607a4ce531893353786e40efd67
soumyaranjanpuhan <soumyaranjan.puhan@33crores.com>
2025-07-25 18:20:34 +0530

    e

--- resources/views/admin/flower-order/manage-flower-orders.blade.php
+++ resources/views/admin/flower-order/manage-flower-orders.blade.php
@@ -13,10 +13,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Select2 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-
-
-
-
     <!-- INTERNAL Select2 css -->
     <link href="{{ asset('assets/plugins/select2/css/select2.min.css') }}" rel="stylesheet" />
     <style>
@@ -42,7 +38,6 @@
             background-color: #45a049;
         }
 
-        /* Action Buttons (Pause/Resume) */
         .btn-action {
             background-color: #c80100;
             box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
