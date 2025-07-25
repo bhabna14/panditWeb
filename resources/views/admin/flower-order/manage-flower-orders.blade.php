0cd7fbc18dfda0b7029c90d18b6623201af512df
soumyaranjanpuhan <soumyaranjan.puhan@33crores.com>
2025-07-25 14:25:26 +0530

    s

--- resources/views/admin/flower-order/manage-flower-orders.blade.php
+++ resources/views/admin/flower-order/manage-flower-orders.blade.php
@@ -11,6 +11,9 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Select2 CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+
 
 
 
@@ -500,7 +503,28 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://cdn.jsdelivr.net/npm/moment@2.29.1/moment.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
+    <script>
+        $(document).ready(function() {
+            $('#customer_name').select2({
+                placeholder: 'Select Customer',
+                allowClear: true
+            });
+            $('#mobile_number').select2({
+                placeholder: 'Select Mobile',
+                allowClear: true
+            });
+            $('#apartment_name').select2({
+                placeholder: 'Select Apartment',
+                allowClear: true
+            });
+            $('#apartment_flat_plot').select2({
+                placeholder: 'Select Flat/Plot',
+                allowClear: true
+            });
+        });
+    </script>
     <script>
         $(function() {
             const table = $('#file-datatable').DataTable({
