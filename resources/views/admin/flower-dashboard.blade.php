fad7b606e465e8d39de533fdf4292c317ed148d5
soumyaranjanpuhan <soumyaranjan.puhan@33crores.com>
2025-07-26 13:00:00 +0530

    d

--- resources/views/admin/flower-dashboard.blade.php
+++ resources/views/admin/flower-dashboard.blade.php
@@ -76,14 +76,14 @@
                             <div class="row">
                                 <div class="col-12">
                                     <div class="ps-4 pt-4 pe-3 pb-4">
-                                        <h5 class="mb-2 tx-12">Today Active Subscription/Total Delivery</h5>
+                                        <h5 class="mb-2 tx-12">Active Subscription/Total Delivery</h5>
                                         <h4 class="tx-20 font-weight-semibold mb-2">
                                             {{ $activeSubscriptions }}/{{ $totalDeliveriesTodayCount }}</h4>
                                     </div>
                                 </div>
                                 <div>
                                     <div
-                                        class="circle-icon bg-gradient-to-r from-teal-500 to-blue-600 text-center align-self-center overflow-hidden">
+                                        class="circle-icon from-teal-500 to-blue-600 text-center align-self-center overflow-hidden">
                                         <i class="fa fa-user tx-16 text-white"></i> <!-- Active Subscription Icon -->
                                     </div>
                                 </div>
