4a05602d37c929ba92f6cdd0cc7f84b90da13a4b
soumyaranjanpuhan <soumyaranjan.puhan@33crores.com>
2025-07-28 14:51:41 +0530

    s

--- resources/views/admin/address-category-summary.blade.php
+++ resources/views/admin/address-category-summary.blade.php
@@ -1,4 +1,4 @@
-@extends('admin.layouts.app')
+@extends('admin.layouts.apps')
 
 @section('styles')
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
