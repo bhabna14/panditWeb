b7fa6173470c1f0fee535a9b500e2bcd832a09f8
soumyaranjanpuhan <soumyaranjan.puhan@33crores.com>
2025-08-20 14:46:04 +0530

    d

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\FlowerProduct;
+use Illuminate\Support\Str;
 use App\Models\Order;
 use Illuminate\Support\Facades\Auth;
 
