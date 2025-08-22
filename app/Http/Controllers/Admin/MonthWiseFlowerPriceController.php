82466f7fbc4d2045278586e90f48309a9a30ca30
soumyaranjanpuhan <soumyaranjan.puhan@33crores.com>
2025-08-22 17:25:07 +0530

    d

--- app/Http/Controllers/Admin/MonthWiseFlowerPriceController.php
+++ app/Http/Controllers/Admin/MonthWiseFlowerPriceController.php
@@ -83,7 +83,6 @@ public function manageFlowerPrice()
 
 public function updateFlowerPrice(Request $request, $id)
 {
-    dd('s');
     $request->validate([
         'start_date'     => 'required|date',
         'end_date'       => 'required|date|after_or_equal:start_date',
