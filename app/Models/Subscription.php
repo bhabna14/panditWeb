7d59f710f9e0a57ee0ca1060363708ac3e7cbb1a
soumyaranjanpuhan <soumyaranjan.puhan@33crores.com>
2025-08-30 12:31:18 +0530

    s

--- app/Models/Subscription.php
+++ app/Models/Subscription.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Subscription extends Model
 {
@@ -81,7 +82,6 @@ class Subscription extends Model
    }
 
    
-use Carbon\Carbon;
 
 public function scopeActiveOn($q, Carbon $date)
 {
