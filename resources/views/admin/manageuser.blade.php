3ee7c7f89802025bb949f59964cd2d648ef9a31a
soumyaranjanpuhan <soumyaranjan.puhan@33crores.com>
2025-09-12 14:25:45 +0530

    s

--- resources/views/admin/manageuser.blade.php
+++ resources/views/admin/manageuser.blade.php
@@ -112,8 +112,8 @@
 
 @section('content')
     <!-- Dashboard Stats -->
-    <div class="row g-3 mb-4 mt-3" style="border: 1px solid rgb(186, 185, 185);">
-        <div class="col-md-3">
+    <div class="row g-3 mb-4 mt-3">
+        <div class="col-md-3" style="border: 1px solid rgb(186, 185, 185);">
             <div class="stats-card d-flex justify-content-between align-items-center border" >
                 <div>
                     <div class="stats-title">Total Customers</div>
