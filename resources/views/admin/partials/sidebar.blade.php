c1e0c1b22334fa1b9723ffe1abd1da685ce26e42
soumyaranjanpuhan <soumyaranjan.puhan@33crores.com>
2025-10-17 18:29:54 +0530

    s

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -534,11 +534,9 @@
                     <path d="M13.293 6.293 7.586 12l5.707 5.707 1.414-1.414L10.414 12l4.293-4.293z" />
                 </svg>
             </div>
-
-            {{-- <ul class="side-menu">
-                <li class="side-item side-item-category">Main</li>
+            <ul class="side-menu">
                 {!! $renderMenu($menuRoots) !!}
-            </ul> --}}
+            </ul>
 
             <div class="slide-right" id="slide-right" aria-hidden="true">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="#7b8191" width="24" height="24" viewBox="0 0 24 24" aria-hidden="true">
